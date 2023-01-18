ca74c27665c7147bb3556f31325466d2516dd31c
Miguel Gutierrez <gt2rz.dev@gmail.com>
2023-01-18 18:00:16 -0500

    add navbar and hero section

---
+++ src/data/topicsFooter.js
@@ -0,0 +1,23 @@
+export const topics = {
+    links:[
+        {name:"Planes", link:"#"},
+        {name:"Servicios", link:"#"},
+        {name:"Preguntas frecuentes", link:"#"},
+        {name:"Ayuda", link:"#"},
+        {name:"Soporte", link:"#"},
+    ],
+    legal:[
+        {name:"Terminos y condiciones", link:"#"},
+        {name:"Privacidad", link:"#"},
+    ],
+    social:[
+        {name:"Facebok", link:"#"},
+        {name:"Linkedin", link:"#"},
+        {name:"Twitter", link:"#"},
+    ],
+    company:[
+        {name:"Blog", link:"#"},
+        {name:"Sobre nosotros", link:"#"},
+        {name:"Contactos", link:"#"},
+    ]
+}
